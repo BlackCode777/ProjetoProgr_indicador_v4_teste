52c72d692d1f267de43c145ae01d043ecbe7525e
Anderson Martins <BlackCode77.1980@gmail.com>
2024-01-12 07:23:55 -0300

    Adicionando import { CdkMenuModule } from '@angular/cdk/menu'

--- ProjProgr_indicador_v4/src/app/app.module.ts
+++ ProjProgr_indicador_v4/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProductsComponent } from './products/products.component';
-import { StatisticsComponent } from './statistics/statistics.component';
 import { CoupensComponent } from './coupens/coupens.component';
-import { PagesComponent } from './pages/pages.component';
-import { MediaComponent } from './media/media.component';
-import { SettingsComponent } from './settings/settings.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { MediaComponent } from './media/media.component';
+import { PagesComponent } from './pages/pages.component';
+import { ProductsComponent } from './products/products.component';
+import { SettingsComponent } from './settings/settings.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+
+import { CdkMenuModule } from '@angular/cdk/menu';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 @NgModule({
   declarations: [
@@ -29,6 +32,8 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
+    OverlayModule ,
+    CdkMenuModule ,
     BrowserModule,
     AppRoutingModule
   ],
