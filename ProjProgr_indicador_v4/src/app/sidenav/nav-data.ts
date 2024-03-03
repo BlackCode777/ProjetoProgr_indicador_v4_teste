24c8c0139955a8036e861d8a37c4fba004f8cc29
Anderson Martins <BlackCode77.1980@gmail.com>
2024-03-03 00:07:20 -0300

    ...

--- ProjProgr_indicador_v4/src/app/sidenav/nav-data.ts
+++ ProjProgr_indicador_v4/src/app/sidenav/nav-data.ts
@@ -1,37 +1,37 @@
 export const navbarData = [
-    {
-        routeLink: 'dashboard',
-        icon: 'fal fa-home',
-        label: 'Indicador Dashboard'
-    },
-    {
-        routeLink: 'produtos',
-        icon: 'fal fa-box-open',
-        label: 'Produtos'
-    },
-    {
-        routeLink: 'estatisticas',
-        icon: 'fal fa-chart-bar',
-        label: 'Statisticas'
-    },
-    {
-        routeLink: 'cupons',
-        icon: 'fal fa-tags',
-        label: 'Cupons'
-    },
-    {
-        routeLink: 'pages',
-        icon: 'fal fa-file',
-        label: 'Pages'
-    },
-    {
-        routeLink: 'medias',
-        icon: 'fal fa-camera',
-        label: 'Medias'
-    },
-    {
-        routeLink: 'configuracoes',
-        icon: 'fal fa-cog',
-        label: 'Configurações'
-    },
+  {
+    routeLink: 'dashboard',
+    icon: 'fal fa-home',
+    label: 'Indicador Dashboard',
+  },
+  {
+    routeLink: 'regioes',
+    icon: 'fa fa-map',
+    label: 'Regiões',
+  },
+  {
+    routeLink: 'orgaos',
+    icon: 'fal fa-building-ngo',
+    label: 'Orgãos',
+  },
+  {
+    routeLink: 'servicos',
+    icon: 'fal fa-tags',
+    label: 'Serviços',
+  },
+  // {
+  //   routeLink: 'pages',
+  //   icon: 'fal fa-file',
+  //   label: 'Pages',
+  // },
+  // {
+  //   routeLink: 'medias',
+  //   icon: 'fal fa-camera',
+  //   label: 'Medias',
+  // },
+  // {
+  //   routeLink: 'configuracoes',
+  //   icon: 'fal fa-cog',
+  //   label: 'Configurações',
+  // },
 ];
